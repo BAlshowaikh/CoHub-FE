2bda143613a5cdacabf352fc7a63bc9d9960711d
BAlshowaikh <batoolalshowaikh04@outlook.com>
2025-12-30 12:12:00 +0300

    Modified navbar -handle both logged in and not logged in users-

--- src/services/api/Projects.api.js
+++ src/services/api/Projects.api.js
@@ -48,7 +48,7 @@ export const createProject = async (data) => {
 export const getProjectAssignees = async (projectId) => {
   try {
     const res = await Client.get(`/project/${projectId}/assignees`)
-    res.data
+    return res.data
   } catch (error) {
     throw error
   }
