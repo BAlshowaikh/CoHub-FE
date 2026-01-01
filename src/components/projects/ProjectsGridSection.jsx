96bd71b207a880a6cf444504bee3217151f94363
Batool Alshowaikh <batoolalshowaikh04@outlook.com>
2026-01-01 12:49:49 +0300

    Update ProjectsGridSection.jsx

--- src/components/projects/ProjectsGridSection.jsx
+++ src/components/projects/ProjectsGridSection.jsx
@@ -1,10 +1,1 @@
-// Wrapper component around tehe project cards 
-const ProjectsGridSection = ({ children }) => {
-  return (
-    <div className="projects-panel">
-      <div className="projects-panel__inner">{children}</div>
-    </div>
-  )
-}
 
-export default ProjectsGridSection
