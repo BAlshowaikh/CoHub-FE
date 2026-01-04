4a23e19673fd91cd6df0e4c8b796c76eff21bef9
BAlshowaikh <batoolalshowaikh04@outlook.com>
2026-01-04 16:23:15 +0300

    Modify api link in project tasks

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -2,7 +2,7 @@
 import { Link } from "react-router-dom"
 const Project = ({ project }) => {
   return (
-    <Link to={`/project/${project._id}`} className="project-card">
+    <Link to={`/project/tasks/${project._id}`} className="project-card">
       <div className="Aproject">
         <h3>{project.name}</h3>
         <p>{project.description}</p>
