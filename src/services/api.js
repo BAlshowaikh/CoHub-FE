18383394aa53b0bc24fbe1dddc0a4369fb294880
BAlshowaikh <batoolalshowaikh04@outlook.com>
2026-01-05 20:40:35 +0300

    added env with BE URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import Axios from "axios"
 
-export const BASE_URL = "https://cohub-be.onrender.com" || "http://localhost:3000" 
+export const BASE_URL = import.meta.env.VITE_APP_BACKEND_URL || "http://localhost:3000" 
 
 const Client = Axios.create({ baseURL: BASE_URL })
 
